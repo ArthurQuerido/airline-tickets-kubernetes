b3280cdaf4d2e524496b2b2d690d97125ca9f6d4
Arthur <arthur_ql@hotmail.com>
2021-11-03 16:08:12 -0300

    Release

---
+++ app-container/app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+from models import Base
+from settings import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, convert_unicode=True)
+
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+                                         
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.query = db_session.query_property()
+
+def init_db():    
+    import models
+    Base.metadata.create_all(bind=engine)
